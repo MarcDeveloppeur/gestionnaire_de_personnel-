ab53157d1143665b9b8b332342302e88a67b189c
Marc <marcramadison006@gmail.com>
2021-03-24 07:54:47 +0300

    Créer la page de connection

--- frontend/src/Pages/ConnectPage.js
+++ frontend/src/Pages/ConnectPage.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {Link} from 'react-router-dom';
 import './PageStyles/ConnectPageCss.css';
 
 function ConnectPage() {
 
+  const [identifiant,setIdentifiant]=useState('');
+  const [motDePasse,setMotDePasse]=useState('');
+
   //La fonction pour connecter l'utilisateur
   const connecter=(e)=>{
     e.preventDefault();
-    console.log('salut');
+    console.log(identifiant+' '+motDePasse);
   }
 
   return (
@@ -15,11 +18,11 @@ function ConnectPage() {
        <div className="ConnectForm">
            <h1 className="formTitle">Connectez-vous</h1>
            <form onSubmit={connecter}>
-               <input type="text" className="textInput" placeholder="Identifiant"/>
-               <input type="password" className="textInput" placeholder="Mot de passe"/>
-               <input type="submit" className="btn-envoyer" value="Connecter"/>
+               <input type="text" value={identifiant} onChange={(e)=>setIdentifiant(e.target.value)} className="textInput" placeholder="Identifiant"/>
+               <input type="password" value={motDePasse} onChange={(e)=>setMotDePasse(e.target.value)} className="textInput" placeholder="Mot de passe"/>
+               <input type="submit" className="btn-connecter" value="Se connecter"/>
            </form>
-           <Link to="/Register">Créer un compte</Link>
+           <Link to="/Register" className="link">Créer un compte</Link>
        </div>
     </div>
   );
