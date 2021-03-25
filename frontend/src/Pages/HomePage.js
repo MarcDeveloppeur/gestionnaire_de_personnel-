75cd105950f22a74c9e52dec4c693d4a882a1d89
Marc <marcramadison006@gmail.com>
2021-03-25 07:59:29 +0300

    Créer la page d'acceuil

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import './PageStyles/globalCss.css';
+import './PageStyles/HomePageCss.css';
 
 function HomePage() {
   return (
-    <div className="App">
-
+    <div className="homeContainer">
+        <div className="HomeContent">
+           <div className="homeTitle">
+               <p>Bienvenu</p>
+               <span className="userName">User_name</span>
+           </div>
+           <div className="linkBlock">
+               <Link to="" className="addLink Link">Nouveau colloborateur</Link>
+               <Link to="" className="listLink Link">Liste de vos colloborateur</Link>
+          </div>
+        </div>
     </div>
   );
 }
